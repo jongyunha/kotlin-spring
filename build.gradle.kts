2fe96263924fd52f362040cde7bf9b7ff30ccf78
neimen <neimen.95@kakaostyle.com>
2022-07-20 20:59:21 +0900

    logging setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,9 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // logging
+    implementation("io.github.microutils:kotlin-logging-jvm:2.1.23")
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/io/jongyun/kotlinspring/controller/GreetingController.kt
+++ src/main/kotlin/io/jongyun/kotlinspring/controller/GreetingController.kt
@@ -1,7 +1,7 @@
 package io.jongyun.kotlinspring.controller
 
 import io.jongyun.kotlinspring.service.GreetingService
-import org.springframework.beans.factory.annotation.Value
+import mu.KLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,9 +16,11 @@ class GreetingController(
     val greetingService: GreetingService
 ) {
 
+    companion object : KLogging()
 
     @GetMapping("/{name}")
     fun retrieveGreeting(@PathVariable("name") name: String): String {
+        logger.info("Name is $name")
         return greetingService.retrieveGreeting(name)
     }
 }
